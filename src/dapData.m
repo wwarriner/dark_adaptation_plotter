dfdaf05e5f842b548f27c42a8a89dbc354af87fa
wwarriner <wwarriner@gmail.com>
2021-05-11 15:36:28 -0500

    ordering improvement

--- src/dapData.m
+++ src/dapData.m
@@ -1,11 +1,12 @@
 classdef dapData < handle
-    properties (SetAccess = private, Dependent)
+    properties (SetAccess = private)
         ids (:,1) string
     end
     
     methods
         function load(obj, file_path)
             obj.patients = containers.Map("keytype", "char", "valuetype", "any");
+            obj.order = containers.Map("keytype", "double", "valuetype", "char");
             
             t = readtable(file_path);
             % TODO mangle t.Properties.VariableNames
@@ -14,7 +15,12 @@ classdef dapData < handle
             t = t(:, sorted_names);
             t.PPT_ID = string(t.PPT_ID);
             
-            for id = unique(t.PPT_ID).'
+            pt_ids = unique(t.PPT_ID);
+            pt_ids = natsort(pt_ids); % https://www.mathworks.com/matlabcentral/fileexchange/47434-natural-order-filename-sort
+            start_order = numel(obj.ids);
+            for i = 1 : numel(pt_ids)
+                id = pt_ids(i);
+                
                 subset = t(t.PPT_ID == id, :);
                 
                 recovery_time = unique(subset.RIT);
@@ -27,6 +33,7 @@ classdef dapData < handle
                     subset.threshold_value1 ...
                     );
                 obj.patients(char(id)) = dp;
+                obj.order(start_order + i) = char(id);
             end
         end
         
@@ -37,14 +44,13 @@ classdef dapData < handle
 
     methods % properties
         function ids = get.ids(obj)
-            ids = obj.patients.keys();
-            ids = string(ids);
-            ids = natsort(ids); % https://www.mathworks.com/matlabcentral/fileexchange/47434-natural-order-filename-sort
+            ids = string(obj.order.values());
         end
     end
     
     properties (Access = private)
         patients containers.Map
+        order containers.Map
     end
     
     properties (Access = private, Constant)

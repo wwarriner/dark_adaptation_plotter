73016395e266fb5e2943b105fad4318641740c35
wwarriner <wwarriner@gmail.com>
2021-05-13 15:32:09 -0500

    added unicode marker list for combo

---
+++ list_markers.m
@@ -0,0 +1,18 @@
+function markers = list_markers()
+
+markers = {...
+    char(9679), ... % black circle UTF-16 0x25cf
+    char(43), ... % plus sign UTF-16 0x2b
+    char(10033), ... % heavy asterisk UTF-16 0x2731
+    char(215), ... % multiplication sign UTF-16 0xd7
+    char(9632), ... % black square UTF-16 0x25a0
+    char(9670), ... % black diamond UTF-16 0x25c6
+    char(9650), ... % black up-pointing triangle UTF-16 0x25b2
+    char(9660), ... % black down-pointing triangle UTF-16 0x25bc
+    char(9654), ... % black right-pointing triangle UTF-16 0x25b6
+    char(9664), ... % black left-pointing triangle UTF-16 0x25c0
+    char(9733) ... % black star UTF-16 0x2605
+    };
+
+end
+

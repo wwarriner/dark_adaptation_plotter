dfe9ed6963ecd74b6a13e72dd4976d11c78c0de9
wwarriner <wwarriner@gmail.com>
2021-05-18 15:36:43 -0500

    integrated changes

--- DarkAdaptationPlotter_code.m
+++ DarkAdaptationPlotter_code.m
@@ -3,23 +3,28 @@ classdef DarkAdaptationPlotter < matlab.apps.AppBase
     % Properties that correspond to app components
     properties (Access = public)
         UIFigure          matlab.ui.Figure
-        FileMenu          matlab.ui.container.Menu
+        FileParentMenu    matlab.ui.container.Menu
         OpenDataMenu      matlab.ui.container.Menu
         ExportFigureMenu  matlab.ui.container.Menu
         ClearDataMenu     matlab.ui.container.Menu
-        PreferencesMenu   matlab.ui.container.Menu
         ExitMenu          matlab.ui.container.Menu
+        EditMenu          matlab.ui.container.Menu
+        FontMenu          matlab.ui.container.Menu
+        PreferencesMenu   matlab.ui.container.Menu
+        HelpParentMenu    matlab.ui.container.Menu
         HelpMenu          matlab.ui.container.Menu
-        HelpMenu_2        matlab.ui.container.Menu
         AboutMenu         matlab.ui.container.Menu
-        PlotTable         matlab.ui.control.Table
+        AxesPanel         matlab.ui.container.Panel
+        Table             matlab.ui.control.Table
     end
     
     properties (Access = private)
         dap_axes = []
+        dap_legend = []
         dap_table = []
         dap_plots = []
         dap_data = []
+        font_settings = []
     end
     
     methods (Access = private)
@@ -54,6 +59,63 @@ classdef DarkAdaptationPlotter < matlab.apps.AppBase
             app.dap_plots.update_marker(id, marker);
         end
         
+        function resize(app)
+            %{
+            Minimum width of 640, height of 480
+            
+            Table and Axes are full height
+            Table has 30%(?) of width, Axes has 70%(?)
+            Table columns are distributed
+            %}
+            position = app.UIFigure.Position;
+            
+            % minimum size
+            %             MIN_SIZE = [640 480];
+            %             position(3) = max(position(3), MIN_SIZE(1));
+            %             position(4) = max(position(4), MIN_SIZE(2));
+            %             app.UIFigure.Position = position;
+            
+            % table
+            TABLE_X_FRACTION = 0.33;
+            table_x = 1;
+            table_y = 1;
+            table_width = round(TABLE_X_FRACTION .* position(3));
+            table_height = position(4);
+            app.Table.Position = [table_x table_y table_width table_height];
+            TABLE_WIDTH_WEIGHTS = [96 32 50 60];
+            table_width_fractions = TABLE_WIDTH_WEIGHTS ./ sum(TABLE_WIDTH_WEIGHTS);
+            table_widths = round(app.Table.Position(3) .* table_width_fractions);
+            table_widths(end) = sum(table_widths) - sum(table_widths(1 : end-1)) - 2;
+            app.Table.ColumnWidth = num2cell(table_widths);
+            
+            % panel
+            panel_x = app.Table.Position(1) + app.Table.Position(3) - 1;
+            panel_y = 1;
+            panel_width = position(3) - table_width;
+            panel_height = position(4);
+            app.AxesPanel.Position = [panel_x panel_y panel_width panel_height];
+            
+            % axes
+            %             AXES_X_FRACTION = 0.6;
+            %             axes_x = 1;
+            %             axes_y = 1;
+            %             axes_width = round(AXES_X_FRACTION .* app.AxesPanel.Position(3));
+            %             axes_height = app.AxesPanel.Position(4);
+            %             app.dap_axes.position = [axes_x axes_y axes_width axes_height];
+            %             app.dap_axes.update_position();
+            
+            % legend
+            %             ip = app.dap_axes.inner_position;
+            %             anchor_x = app.AxesPanel.Position(3) - axes_width;
+            %             legend_x = anchor_x;
+            %             legend_y = ip(2);
+            %             legend_width = (app.AxesPanel.Position(3) - anchor_x) .* 0.75;
+            %             legend_height = ip(4);
+            %             app.dap_legend.position = [legend_x legend_y legend_width legend_height];
+            %             app.dap_legend.position(1) = legend_x;
+            %             app.dap_legend.update_position();
+        end
+        
         function close(app)
             delete(app);
         end
@@ -66,26 +128,36 @@ classdef DarkAdaptationPlotter < matlab.apps.AppBase
         function startupFcn(app)
             extend_search_path();
             
-            pos = app.UIFigure.Position;
-            pos(1) = app.PlotTable.Position(1) + app.PlotTable.Position(3) - 1;
-            pos(3) = pos(3) - pos(1);
-            pos(2) = 0;
-            da = dapAxes(app.UIFigure);
-            da.position = pos;
-            da.update_position();
+            layout = tiledlayout(app.AxesPanel, 1, 3);
+            layout.Padding = "tight";
+            layout.TileSpacing = "tight";
+            
+            da = dapAxes(app.AxesPanel, layout);
             
             recovery_line = dapRecoveryLine();
             da.draw_on(@recovery_line.draw);
             da.update();
             
+            dl = dapLegend(da, layout);
+            
             dd = dapData();
-            dt = dapPlotTable(app.PlotTable);
+            dt = dapPlotTable(app.Table);
             dp = dapPlots(da);
             
+            % TODO config defaults
+            font_opt = FontSettings("Helvetica", 24, "normal", "normal");
+            font_opt.register(app.Table);
+            font_opt.register(da, @da.update_font);
+            font_opt.update();
+            
             app.dap_axes = da;
+            app.dap_legend = dl;
             app.dap_data = dd;
             app.dap_table = dt;
             app.dap_plots = dp;
+            app.font_settings = font_opt;
+            
+            resize(app);
         end
         
         % Size changed function: UIFigure
@@ -94,17 +166,7 @@ classdef DarkAdaptationPlotter < matlab.apps.AppBase
                 return;
             end
             
-            position = app.UIFigure.Position;
-            
-            pos = position;
-            fudge = 100;
-            pos(1) = app.PlotTable.Position(1) + app.PlotTable.Position(3) - 1 - fudge;
-            pos(3) = min(pos(3) - pos(1), pos(4) + app.PlotTable.Position(3));
-            pos(2) = 0;
-            app.dap_axes.position = pos;
-            app.dap_axes.update_position();
-            
-            app.PlotTable.Position = [1 1 app.PlotTable.Position(3) position(4)];
+            resize(app);
         end
         
         % Menu selected function: OpenDataMenu
@@ -128,8 +190,8 @@ classdef DarkAdaptationPlotter < matlab.apps.AppBase
             app.dap_plots.add(patients);
         end
         
-        % Cell edit callback: PlotTable
-        function PlotTableCellEdit(app, event)
+        % Cell edit callback: Table
+        function TableCellEdit(app, event)
             %{
             Only the visibility and marker columns are directly editable.
             For color editing see PlotTableCellSelection().
@@ -146,8 +208,8 @@ classdef DarkAdaptationPlotter < matlab.apps.AppBase
             end
         end
         
-        % Cell selection callback: PlotTable
-        function PlotTableCellSelection(app, event)
+        % Cell selection callback: Table
+        function TableCellSelection(app, event)
             %{
             A way to allow editing of color column using a picker.
             %}
@@ -199,11 +261,18 @@ classdef DarkAdaptationPlotter < matlab.apps.AppBase
             % pdf, eps, png
         end
         
-        % Menu selected function: PreferencesMenu
+        % Callback function
         function PreferencesMenuSelected(app, event)
             % TODO bring up preferences dialog
             % export options - png image size
             %
+        end
+        
+        % Menu selected function: FontMenu
+        function FontMenuSelected(app, event)
+            assert(~isempty(app.font_settings));
+            
+            app.font_settings.ui_get();
         end
     end
     
@@ -221,60 +290,72 @@ classdef DarkAdaptationPlotter < matlab.apps.AppBase
             app.UIFigure.CloseRequestFcn = createCallbackFcn(app, @UIFigureCloseRequest, true);
             app.UIFigure.SizeChangedFcn = createCallbackFcn(app, @UIFigureSizeChanged, true);
             
-            % Create FileMenu
-            app.FileMenu = uimenu(app.UIFigure);
-            app.FileMenu.Text = 'File';
+            % Create FileParentMenu
+            app.FileParentMenu = uimenu(app.UIFigure);
+            app.FileParentMenu.Text = 'File';
             
             % Create OpenDataMenu
-            app.OpenDataMenu = uimenu(app.FileMenu);
+            app.OpenDataMenu = uimenu(app.FileParentMenu);
             app.OpenDataMenu.MenuSelectedFcn = createCallbackFcn(app, @OpenDataMenuSelected, true);
             app.OpenDataMenu.Text = 'Open Data...';
             
             % Create ExportFigureMenu
-            app.ExportFigureMenu = uimenu(app.FileMenu);
+            app.ExportFigureMenu = uimenu(app.FileParentMenu);
             app.ExportFigureMenu.MenuSelectedFcn = createCallbackFcn(app, @ExportFigureMenuSelected, true);
             app.ExportFigureMenu.Text = 'Export Figure...';
             
             % Create ClearDataMenu
-            app.ClearDataMenu = uimenu(app.FileMenu);
+            app.ClearDataMenu = uimenu(app.FileParentMenu);
             app.ClearDataMenu.MenuSelectedFcn = createCallbackFcn(app, @ClearDataMenuSelected, true);
             app.ClearDataMenu.Text = 'Clear Data...';
             
-            % Create PreferencesMenu
-            app.PreferencesMenu = uimenu(app.FileMenu);
-            app.PreferencesMenu.MenuSelectedFcn = createCallbackFcn(app, @PreferencesMenuSelected, true);
-            app.PreferencesMenu.Separator = 'on';
-            app.PreferencesMenu.Text = 'Preferences...';
-            
             % Create ExitMenu
-            app.ExitMenu = uimenu(app.FileMenu);
+            app.ExitMenu = uimenu(app.FileParentMenu);
             app.ExitMenu.MenuSelectedFcn = createCallbackFcn(app, @ExitMenuSelected, true);
             app.ExitMenu.Separator = 'on';
             app.ExitMenu.Text = 'Exit';
             
-            % Create HelpMenu
-            app.HelpMenu = uimenu(app.UIFigure);
-            app.HelpMenu.Text = 'Help';
+            % Create EditMenu
+            app.EditMenu = uimenu(app.UIFigure);
+            app.EditMenu.Text = 'Edit';
             
-            % Create HelpMenu_2
-            app.HelpMenu_2 = uimenu(app.HelpMenu);
-            app.HelpMenu_2.Text = 'Help...';
+            % Create FontMenu
+            app.FontMenu = uimenu(app.EditMenu);
+            app.FontMenu.MenuSelectedFcn = createCallbackFcn(app, @FontMenuSelected, true);
+            app.FontMenu.Text = 'Font...';
+            
+            % Create PreferencesMenu
+            app.PreferencesMenu = uimenu(app.EditMenu);
+            app.PreferencesMenu.Text = 'Preferences...';
+            
+            % Create HelpParentMenu
+            app.HelpParentMenu = uimenu(app.UIFigure);
+            app.HelpParentMenu.Text = 'Help';
+            
+            % Create HelpMenu
+            app.HelpMenu = uimenu(app.HelpParentMenu);
+            app.HelpMenu.Text = 'Help...';
             
             % Create AboutMenu
-            app.AboutMenu = uimenu(app.HelpMenu);
+            app.AboutMenu = uimenu(app.HelpParentMenu);
             app.AboutMenu.Separator = 'on';
             app.AboutMenu.Text = 'About...';
             
-            % Create PlotTable
-            app.PlotTable = uitable(app.UIFigure);
-            app.PlotTable.ColumnName = {'ID'; '👁️'; 'Color'; 'Marker'};
-            app.PlotTable.ColumnWidth = {96, 32, 50, 60};
-            app.PlotTable.RowName = {};
-            app.PlotTable.ColumnSortable = [true true false false];
-            app.PlotTable.ColumnEditable = [false true true true];
-            app.PlotTable.CellEditCallback = createCallbackFcn(app, @PlotTableCellEdit, true);
-            app.PlotTable.CellSelectionCallback = createCallbackFcn(app, @PlotTableCellSelection, true);
-            app.PlotTable.Position = [1 1 240 600];
+            % Create Table
+            app.Table = uitable(app.UIFigure);
+            app.Table.ColumnName = {'ID'; '👁️'; 'Color'; 'Marker'};
+            app.Table.ColumnWidth = {96, 32, 50, 60};
+            app.Table.RowName = {};
+            app.Table.ColumnSortable = [true true false false];
+            app.Table.ColumnEditable = [false true true true];
+            app.Table.CellEditCallback = createCallbackFcn(app, @TableCellEdit, true);
+            app.Table.CellSelectionCallback = createCallbackFcn(app, @TableCellSelection, true);
+            app.Table.Position = [1 1 239.754098360656 600];
+            
+            % Create AxesPanel
+            app.AxesPanel = uipanel(app.UIFigure);
+            app.AxesPanel.AutoResizeChildren = 'off';
+            app.AxesPanel.Position = [240 1 961 600];
             
             % Show the figure after all components are created
             app.UIFigure.Visible = 'on';

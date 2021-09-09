007a9db8ea30eac975fcf0f66137fe12703057fd
wwarriner <wwarriner@gmail.com>
2021-09-09 15:10:55 -0500

    increased preview/export robust

--- dapOutputFiles.m
+++ dapOutputFiles.m
@@ -10,30 +10,44 @@ classdef dapOutputFiles < handle
             obj.dap_axes = dap_axes;
         end
         
-        function ui_run_export_preview(obj)
+        function ui_run_export_preview(obj, figure_for_dialogs)
             fh = obj.generate_figure();
             fh.Visible = "on";
             
             menuh = uimenu(fh);
-            menuh.MenuSelectedFcn = @(varargin)obj.ui_run_export_as();
+            menuh.MenuSelectedFcn = @(varargin)obj.ui_run_export_as(figure_for_dialogs);
             menuh.Text = 'Export As...';
+            
+            d = uiprogressdlg(figure_for_dialogs);
+            d.Message = "Previewing figure for export...";
+            d.Title = "Previewing";
+            d.Indeterminate = true;
+            closer = onCleanup(@()d.close());
+            
+            uiwait(fh);
         end
         
-        function ui_run_export_as(obj)
+        function ui_run_export_as(obj, figure_for_dialogs)
+            d = uiprogressdlg(figure_for_dialogs);
+            d.Message = "Exporting figure...";
+            d.Title = "Exporting";
+            d.Indeterminate = true;
+            closer = onCleanup(@()d.close());
+            
             filter = ["*.png"; "*.eps"; "*.pdf"];
             title = "Export figure as";
             default_file_path = fullfile(obj.folder, obj.file_name);
-            [file, path] = uiputfile(filter, title, default_file_path);
-            if file == 0
+            [name, path] = uiputfile(filter, title, default_file_path);
+            if name == 0
                 return;
             end
+            obj.folder = string(path);
+            obj.file_name = string(name);
+            file = fullfile(path, name);
             
             fh = obj.generate_figure();
-            closer = onCleanup(@()delete(fh));
-            exportgraphics(fh, fullfile(path, file));
-            obj.folder = string(path);
-            obj.file_name = string(file);
-            delete(closer);
+            deleter = onCleanup(@()delete(fh));
+            exportgraphics(fh, file);
         end
     end
     
